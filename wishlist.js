95ba2ca8ca5e1dcb40e7b6b56c307b7ad21dd213
VolodymyrZin <vl.zinych@gmail.com>
2026-06-30 19:26:36 +0000

    Deploying to gh-pages from @ VolodymyrZin/storyClassWork@3dc3b464e14f30363c73b2ab175b1aebd45dbcd6 🚀

---
+++ wishlist.js
@@ -0,0 +1,2 @@
+import"./assets/styles-BgKZm0NV.js";import{i as d,r as t,h as i,a as e,b as a}from"./assets/handlers-kLmMz-Hm.js";import"./assets/vendor-Cbhu4xvy.js";document.addEventListener("DOMContentLoaded",d);t.productsList.addEventListener("click",i);t.addToCartBtn.addEventListener("click",e);t.addToWishlistBtn.addEventListener("click",a);
+//# sourceMappingURL=wishlist.js.map
